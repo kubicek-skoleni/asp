4a2b879a506f73fcc8596cf5eeb5c766e4516557
LukasKubicek <kubicek.brno@gmail.com>
2023-11-01 14:29:41 +0100

    nasobilka

---
+++ UkazkoveAplikace/UkazkoveMVC/Controllers/NasobilkaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UkazkoveMVC.Models;
+
+namespace UkazkoveMVC.Controllers
+{
+    public class NasobilkaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var nasobilka = new NasobilkaXY(5);
+
+            return View(nasobilka);
+        }
+    }
+}
---
+++ UkazkoveAplikace/UkazkoveMVC/Models/NasobilkaXY.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace UkazkoveMVC.Models
+{
+    public class NasobilkaXY
+    {
+        public List<List<int>> Rows { get; set; } 
+                = new List<List<int>>();   
+        
+        public NasobilkaXY(int size) 
+        {
+            for(int i = 0;  i < size; i++)
+            {
+                List<int> row = new List<int>();
+
+                for(int j = 0; j < size; j++)
+                {
+                    row.Add( ((i + 1) * (j + 1)) );
+                }
+
+                Rows.Add(row);
+            }
+        }
+
+    }
+}
