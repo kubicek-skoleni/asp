d24906164218aa7969011782c02df926109f3247
LukasKubicek <kubicek.brno@gmail.com>
2023-11-01 13:09:41 +0100

    MVC

---
+++ UkazkoveAplikace/UkazkoveMVC/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UkazkoveMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Index()
+        {
+            return "hello world";
+        }
+    }
+}
