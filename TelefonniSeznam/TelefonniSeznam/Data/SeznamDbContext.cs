040f4d172fad2b86112ce668f1cd823f8078cd5b
LukasKubicek <kubicek.brno@gmail.com>
2023-11-02 11:39:52 +0100

    mvc telefonni seznam

---
+++ TelefonniSeznam/TelefonniSeznam/Data/SeznamDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
+using TelefonniSeznam.Models;
+
+namespace TelefonniSeznam.Data
+{
+    public class SeznamDbContext : DbContext
+    {
+        public DbSet<Clovek> Lide { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conn = "Server=(localdb)\\mssqllocaldb;Database=TelSeznamDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(conn);
+        }
+    }
+}
---
+++ TelefonniSeznam/TelefonniSeznam/Models/Clovek.cs
@@ -0,0 +1,15 @@
+﻿namespace TelefonniSeznam.Models
+{
+    public class Clovek
+    {
+        public int Id { get; set; }
+
+        public string Jmeno { get; set; }
+
+        public string Prijmeni { get; set; }
+
+        public DateTime DatumNarozeni { get; set; }
+
+        public string Telefon { get; set; }
+    }
+}
