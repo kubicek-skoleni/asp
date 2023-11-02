32dc10e8722502011f9528dd9188f5db8972c361
LukasKubicek <kubicek.brno@gmail.com>
2023-11-02 14:35:54 +0100

    api

---
+++ API/WebAPI/WebAPI/Controllers/SkolniRokController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SkolniRokController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public DateOnly Zacatek()
+        {
+            return new DateOnly(2023,9,1);
+        }
+
+        [HttpGet("[action]")]
+        public DateOnly Konec()
+        {
+            return new DateOnly(2024, 6, 30);
+        }
+    }
+}
