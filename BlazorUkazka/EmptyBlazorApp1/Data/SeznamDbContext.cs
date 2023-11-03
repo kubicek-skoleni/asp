68239a6e2bbcb6901c718e5f33f9e870cc5e75da
LukasKubicek <kubicek.brno@gmail.com>
2023-11-03 09:38:47 +0100

    blazor empty

---
+++ BlazorUkazka/EmptyBlazorApp1/Data/SeznamDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmptyBlazorApp1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmptyBlazorApp1.Data
+{
+    public class SeznamDbContext : DbContext
+    {
+        public DbSet<Clovek> Lide { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conn = "Server=(localdb)\\mssqllocaldb;Database=TelSeznamDb;Trusted_Connection=True;MultipleActiveResultSets=true";
+            optionsBuilder.UseSqlServer(conn);
+        }
+    }
+}
---
+++ BlazorUkazka/EmptyBlazorApp1/Models/Clovek.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmptyBlazorApp1.Models
+{
+    public class Clovek
+    {
+        public int Id { get; set; }
+
+        [MaxLength(10)]
+        public string Jmeno { get; set; }
+
+        [MaxLength(10)]
+        public string Prijmeni { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DatumNarozeni { get; set; }
+
+        [MaxLength(12)]
+        public string Telefon { get; set; }
+    }
+}
